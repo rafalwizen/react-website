fc7d44853c84e332aa166eda4f9349a47ea128a6
Rafal Wizen <rafal.wizen@gmail.com>
2024-07-30 20:57:24 +0200

    Finish CardItem and Card components

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,10 +1,12 @@
 import '../../App.css'
 import {HeroSection} from "../HeroSection.jsx";
+import Cards from "../Cards.jsx";
 
 export const Home = () => {
     return (
         <div>
             <HeroSection/>
+            <Cards/>
         </div>
     );
 };
